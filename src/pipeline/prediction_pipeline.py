e3e8de5db078276f660025844ffbb207cc4df695
Sagnik <sagnik.sarkar22-24@bibs.co.in>
2024-04-18 16:53:09 +0530

    code completed and app.py created

---
+++ src/pipeline/prediction_pipeline.py
@@ -0,0 +1,94 @@
+import sys 
+import os 
+from src.exception import CustomException 
+from src.logger import logging 
+from src.utils import load_obj, calculate_haversine_distance
+import pandas as pd
+
+class PredictPipeline: 
+    def __init__(self) -> None:
+        pass
+
+    def predict(self, features): 
+        try: 
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            model_path = os.path.join('artifacts', "model.pkl")
+
+            preprocessor = load_obj(preprocessor_path)
+            model = load_obj(model_path)
+
+            data_scaled = preprocessor.transform(features)
+            pred = model.predict(data_scaled)
+            return pred
+        except Exception as e: 
+            logging.info("Error occured in predict function in prediction_pipeline location")
+            raise CustomException(e,sys)
+        
+class CustomData: 
+        def __init__(self, vendor_id:int,
+                     pickup_datetime:str, 
+                     dropoff_datetime:str, 
+                     passenger_count:int, 
+                     pickup_longitude:float, 
+                     pickup_latitude:float, 
+                     dropoff_longitude:float, 
+                     dropoff_latitude:float): 
+             self.vendor_id = vendor_id
+             self.pickup_datetime = pickup_datetime
+             self.dropoff_datetime = dropoff_datetime
+             self.passenger_count = passenger_count 
+             self.pickup_longitude = pickup_longitude
+             self.pickup_latitude = pickup_latitude
+             self.dropoff_longitude = dropoff_longitude 
+             self.dropoff_latitude = dropoff_latitude 
+
+        def data_transform(self): 
+             try: 
+                  custom_data_input_dict = { 
+                       'vendor_id': [self.vendor_id], 
+                       'pickup_datetime': [self.pickup_datetime], 
+                       'dropoff_datetime': [self.dropoff_datetime],
+                       'passenger_count':[self.passenger_count],
+                       'pickup_longitude':[self.pickup_longitude], 
+                       'pickup_latitude': [self.pickup_latitude], 
+                       'dropoff_longitude': [self.dropoff_longitude], 
+                       'dropoff_latitude': [self.dropoff_latitude]
+                  }
+
+                  df = pd.DataFrame(custom_data_input_dict)
+                  logging.info("Dataframe created")
+
+                  logging.info("Data transformation started")
+                  df['distance'] = calculate_haversine_distance(df['pickup_latitude'],
+                                           df['pickup_longitude'],
+                                           df['dropoff_latitude'],
+                                           df['dropoff_longitude'])
+
+                  #Converting pickup_datetime and dropoff_datetime to datetime type 
+                  df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'], dayfirst=True) 
+                  df['dropoff_datetime'] = pd.to_datetime(df['dropoff_datetime'], dayfirst=True) 
+                  
+                  #day(0 = Monday, 1 = Tuesday..)
+                  df['day_of_the_week'] = df['pickup_datetime'].dt.day_of_week.astype(object)  
+
+                  #month (1 = January...6 = June)
+                  df['month'] = df['pickup_datetime'].dt.month.astype(object) 
+
+                  #hour(0 = 12am, 1 = 1am ... 23 = 11pm)                 
+                  df['hour'] = df['pickup_datetime'].dt.hour.astype(object)  
+
+                  #Extracting time from pickup_datetime
+                  df['pickup_time'] = df['pickup_datetime'].dt.time                         
+
+                  # Calculating duration of trip in hours
+                  df['calculated_duration'] = (df['dropoff_datetime'] - df['pickup_datetime']).dt.total_seconds() / 3600
+                  
+                  #New feature speed, engineered from distance and calculated_duration
+                  df['speed'] = df['distance'] / df['calculated_duration']                  
+                  
+                  logging.info("Dataframe transformation complete")
+                  return df
+             
+             except Exception as e:
+                  logging.info("Error occured in data_transform function in prediction_pipeline")
+                  raise CustomException(e,sys)       
---
+++ src/utils.py
@@ -0,0 +1,91 @@
+import os 
+import sys 
+import pickle 
+from sqlalchemy import create_engine
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+from src.logger import logging
+
+@dataclass
+class ConnectDBConfig(): 
+        host = 'localhost'
+        user = 'root'
+        password = 'Sagnik123#'
+        database = 'nyc_taxi_trips_database'
+        table_name = 'nyc_taxi_trips_data'
+        dataset_path:str = os.path.join('dataset', 'nyc_taxi_data.csv')
+
+class ConnectDB():     
+    def __init__(self):
+         self.connect_db_config = ConnectDBConfig()  
+      
+    def retrieve_data(self):
+        try:
+            logging.info('Initiating Database Connection')
+            engine = create_engine(f'mysql+mysqlconnector://{self.connect_db_config.user}:{self.connect_db_config.password}@{self.connect_db_config.host}/{self.connect_db_config.database}')
+            query = f"SELECT * From {self.connect_db_config.table_name}"
+            df = pd.read_sql(query, engine)
+            os.makedirs(os.path.dirname(self.connect_db_config.dataset_path),exist_ok=True)
+            df.to_csv(self.connect_db_config.dataset_path,index=False)
+            logging.info('Copy of Dataset stored in dataset folder as a csv file')
+        except Exception as e: 
+            raise CustomException(e,sys)
+        finally:
+                engine.dispose()
+                logging.info('Database connection closed')
+        
+def save_function(file_path, obj): 
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok= True)
+        with open (file_path, "wb") as file_obj: 
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)         
+
+def model_performance(X_train, y_train, X_test, y_test, models): 
+    try: 
+        report = {}
+        for i in range(len(models)): 
+            model = list(models.values())[i]
+# Train models
+            model.fit(X_train, y_train)
+# Test data
+            y_test_pred = model.predict(X_test)
+            #R2 Score 
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+
+    except Exception as e: 
+        raise CustomException(e,sys)
+
+# Function to load a particular object 
+def load_obj(file_path):
+    try: 
+        with open(file_path, 'rb') as file_obj: 
+            return pickle.load(file_obj)
+    except Exception as e: 
+        logging.info("Error in load_object fuction in utils")
+        raise CustomException(e,sys)
+
+def calculate_haversine_distance(lat1, lng1, lat2, lng2):
+
+    AVG_EARTH_RADIUS = 6371  # Average radius of the earth in km
+    
+    # Converting latitude and longitude from degrees to radians
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+
+    # Computing the differences in coordinates
+    dlat = lat2 - lat1
+    dlng = lng2 - lng1
+
+    # Haversine formula used to calculate the distance between the coordinates
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlng / 2) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    h = AVG_EARTH_RADIUS * c
+
+    return h
