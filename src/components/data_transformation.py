360d46f395d2b748d746e0997d34e26cbe5950d2
Sagnik <sagnik.sarkar22-24@bibs.co.in>
2024-04-19 13:53:47 +0530

    final commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from sklearn.preprocessing import StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
